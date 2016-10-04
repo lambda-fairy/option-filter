b6975f1dd92fc88ea07d70edcf12ee55edda2594
Chris Wong <lambda.fairy@gmail.com>
2016-10-05 11:43:16 +1300

    Fix documentation

--- lib.rs
+++ lib.rs
@@ -1,27 +1,29 @@
-/// This crate adds a `.filter()` method to `Option<T>`.
-///
-/// To use it, add `option-filter` to your `Cargo.toml`:
-///
-/// ```toml
-/// [dependencies]
-/// option-filter = "1.0"
-/// ```
-///
-/// Then import the extension trait:
-///
-/// ```rust,ignore
-/// extern crate option_filter;
-/// use option_filter::OptionFilterExt;
-/// ```
-///
-/// Now you can filter your `Option`s!
-///
-/// ```rust
-/// # use option_filter::OptionFilterExt;
-/// let answer = Some(42);
-/// assert_eq!(answer.filter(|x| *x == 42), Some(42));
-/// assert_eq!(answer.filter(|x| *x == 43), None);
-/// ```
+#![deny(missing_docs)]
+
+//! This crate adds a `.filter()` method to `Option<T>`.
+//!
+//! To use it, add `option-filter` to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! option-filter = "1.0"
+//! ```
+//!
+//! Then import the extension trait:
+//!
+//! ```rust,ignore
+//! extern crate option_filter;
+//! use option_filter::OptionFilterExt;
+//! ```
+//!
+//! Now you can filter your `Option`s!
+//!
+//! ```rust
+//! # use option_filter::OptionFilterExt;
+//! let answer = Some(42);
+//! assert_eq!(answer.filter(|x| *x == 42), Some(42));
+//! assert_eq!(answer.filter(|x| *x == 43), None);
+//! ```
 
 /// Extension trait for adding a `.filter()` method to `Option<T>`.
 ///
@@ -33,6 +35,8 @@ pub trait OptionFilterExt {
     /// This is given an unwieldy name so that it's unlikely to conflict
     /// with other associated types.
     type OptionFilterInner;
+
+    /// Filters the element of an `Option`.
     fn filter<F: FnOnce(&Self::OptionFilterInner) -> bool>(self, F) -> Self;
 }
 
